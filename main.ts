43f0adc3de6e38dde7df09a0df7ae49b38ce7fad
graef <simonjn45@gmail.com>
2023-12-20 11:47:57 +0100

    json fixed

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ const key = secrets.steam.web_api_key;
 
 const stapil = new Stapil(key);
 
-const apiListData = await stapil.ISteamWebAPIUtil.GetSupportedAPIList(false);
+const apiListData = await stapil.ISteamWebAPIUtil.GetSupportedAPIList(true);
 const list = apiListData.apilist.interfaces.filter(element => {
   // Just check if any name has a number in it
   // mostly because any interface with an number in it is a
@@ -14,5 +14,5 @@ const list = apiListData.apilist.interfaces.filter(element => {
   // that we do not need
   return !/\d/.test(element.name);
 });
-const data = JSON.stringify(list, null, 4);
+const data = JSON.stringify(list, null, 2);
 await Deno.writeTextFile("./ISteamWebAPIUtil.json", data);
